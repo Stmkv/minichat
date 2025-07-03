d159f83770563c31d6cbfc61e52e78c77f445a25
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-07-03 14:40:56 +0300

    feat(argpare confog and options)

--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
+import argparse
 import asyncio
 from datetime import datetime
 
 import aiofiles
 import configargparse
 
-CHAT_HOST = "minechat.dvmn.org"
-CHAT_PORT = 5000
 MAX_READE_BYTES = 100
 
 
-async def write_file(message):
-    async with aiofiles.open("message.txt", "a") as file:
+async def write_file(message, file_path: str) -> None:
+    async with aiofiles.open(file_path, "a") as file:
         time_now = datetime.today().strftime("[%d.%m.%Y %H:%M]")
         await file.write(f"{time_now} {message}")
 
 
-async def tcp_echo_client():
+async def tcp_echo_client(args: argparse.Namespace) -> None:
     reader, writer = await asyncio.open_connection(
-        host=CHAT_HOST,
-        port=CHAT_PORT,
+        host=args.host,
+        port=args.port,
     )
     try:
         while True:
@@ -28,7 +27,7 @@ async def tcp_echo_client():
             try:
                 decode_message = data.decode(encoding="utf-8")
                 print(decode_message.replace("\n", ""))
-                await write_file(decode_message)
+                await write_file(decode_message, args.history)
             except UnicodeDecodeError:
                 continue
     finally:
@@ -36,8 +35,14 @@ async def tcp_echo_client():
         await writer.wait_closed()
 
 
-async def main():
-    await tcp_echo_client()
+async def main() -> None:
+    parser = configargparse.ArgParser(default_config_files=['argparse_config.txt'])
+    parser.add('--host', help='host to connect', env_var='MYAPP_HOST', default='minechat.dvmn.org')
+    parser.add('--port', type=int, help='port number', default=5000)
+    parser.add('--history', help='history file', default='minechat.history')
+    args = parser.parse_args()
+
+    await tcp_echo_client(args)
 
 
 try:
