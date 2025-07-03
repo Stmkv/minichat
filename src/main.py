a69466413b6fb9cd8d629af19c9dbbfc3c56e732
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-07-03 14:11:23 +0300

    feat(Save messages history to file.)

--- src/main.py
+++ src/main.py
@@ -1,27 +1,36 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 CHAT_HOST = 'minechat.dvmn.org'
-CHAT_PORT = '5000'
+CHAT_PORT = 5000
 MAX_READE_BYTES = 100
+
+
+async def write_file(message):
+    async with aiofiles.open('message.txt', 'a') as file:
+        time_now = datetime.today().strftime("[%d.%m.%Y %H:%M]")
+        await file.write(f"{time_now} {message}")
+
 
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
         host=CHAT_HOST,
         port=CHAT_PORT,
     )
-    while True:
-        data = await reader.read(MAX_READE_BYTES)
-        try:
-            decode_data = data.decode(encoding="utf-8")
-            print(decode_data.replace('\n', ''))
-        except UnicodeDecodeError:
-            continue
-        if not data:
-            break
-
-    print('Close the connection')
-    writer.close()
-    await writer.wait_closed()
+    try:
+        while True:
+            data = await reader.read(MAX_READE_BYTES)
+            if not data:
+                break
+            try:
+                decode_message = data.decode(encoding="utf-8")
+                await write_file(decode_message)
+            except UnicodeDecodeError:
+                continue
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 
 async def main():
